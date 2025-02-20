215d03e85c26e88bd0c2b9cf482141a733be34f5
alondra374 <53309322+alondra374@users.noreply.github.com>
2025-02-20 17:19:47 -0600

    Calculadora

---
+++ practica1/Main.java
@@ -0,0 +1,88 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+    int opcion = 0;
+    Scanner calcula = new Scanner(System.in);
+
+    while (opcion != 4){ 
+    
+
+        System.out.println("|========================|");
+        System.out.println("|                        |");
+        System.out.println("| C A L C U L A D O R A  |");
+        System.out.println("|                        |");
+        System.out.println("| Q U E   C A L C U L A  |");
+        System.out.println("|                        |");
+        System.out.println("|========================|");
+        System.out.println("|                        |");
+        System.out.println("|¿ Que deseas realizar ? |");
+        System.out.println("                        ");
+
+        System.out.println("1.- Quiero SUMAR");
+        System.out.println("2.- Quiero MULTIPLICAR");
+        System.out.println("3.- Quiero DIVIDIR");
+        System.out.println("4.- Quiero SALIR");
+
+        opcion = calcula.nextInt();
+
+        /*
+         * aqui van las lineas de scanner
+         * 
+         */
+
+        if (opcion < 1 || opcion > 4 ){
+            System.out.print("opcion no valida");
+        }
+        else{
+
+            if (opcion == 1){
+
+                System.out.println("Ingresa el número.");
+                double a = calcula.nextDouble();
+            
+                System.out.println("Ingresa el otro número.");
+                double b = calcula.nextDouble();
+            
+                double suma = a + b;
+        
+                System.out.println("El resultado de la operacion es  : " + (a + b));    
+                
+
+            }
+
+            if (opcion == 2){
+
+                System.out.println("Ingresa el número.");
+                double c = calcula.nextDouble();
+        
+                System.out.println("Ingresa el otro número.");
+                double d = calcula.nextDouble();
+        
+                double multiplicacion = c * d;
+            
+                System.out.println("El resultado de la operacion es  : " + (c * d));
+                    
+            }
+
+            if (opcion == 3){
+
+                System.out.println("Ingresa el número.");
+                double e = calcula.nextDouble();
+            
+                System.out.println("Ingresa el otro número.");
+                double f = calcula.nextDouble();
+            
+                double divicion = e * f;
+                
+                System.out.println("El resultado de la operacion es  : " + (e * f));
+                
+            }
+
+
+
+        }
+    }
+    }
+}
